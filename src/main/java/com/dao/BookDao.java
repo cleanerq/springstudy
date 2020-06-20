4865015e782af83273a288e8943d27ca19d934ea
cleanerq <qbyun130@163.com>
2020-06-20 21:09:24 +0800

    import

--- src/main/java/com/dao/BookDao.java
+++ src/main/java/com/dao/BookDao.java
@@ -1,6 +1,10 @@
 package com.dao;
 
 import com.bean.Book;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.scheduling.support.SimpleTriggerContext;
 import org.springframework.stereotype.Repository;
 
 /**
@@ -10,8 +14,46 @@ import org.springframework.stereotype.Repository;
 @Repository
 public class BookDao extends BaseDao<Book> {
 
+    @Autowired
+    private NamedParameterJdbcTemplate namedParameterJdbcTemplateB;
+
+    @Autowired
+    private JdbcTemplate jdbcTemplateB;
+
     @Override
     public void save() {
         System.out.println("保存图书");
+    }
+
+
+    /**
+     * 减余额
+     * 减去某个用户的余额
+     */
+    public void updateBalance(String username, Double price) {
+        String sql = "UPDATE account set balance = balance - :price where username = :username";
+
+        jdbcTemplateB.update(sql, price, username);
+    }
+
+    /**
+     * 获取某本图书的价格
+     *
+     * @return
+     */
+    public Double getPrice(String isbn) {
+        String sql = "SELECT t.price from book t where t.isbn = ?";
+
+        Double aDouble = jdbcTemplateB.queryForObject(sql, Double.class, isbn);
+        return aDouble;
+    }
+
+    /**
+     * 减库存，减去某本书的库存，为了简单期间
+     */
+    public void updateStock(String isbn) {
+        String sql = "update book_stock t set t.stock = t.stock - 1 where t.isbn = ?";
+        jdbcTemplateB.update(sql, isbn);
+
     }
 }
--- src/main/java/com/service/BookService.java
+++ src/main/java/com/service/BookService.java
@@ -1,6 +1,8 @@
 package com.service;
 
 import com.bean.Book;
+import com.dao.BookDao;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 /**
@@ -9,4 +11,20 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class BookService extends BaseService<Book> {
+    @Autowired
+    private BookDao bookDao;
+
+    /**
+     * 用户买书操作
+     * @param username
+     * @param isbn
+     */
+    public void checkout(String username, String isbn) {
+        // 减库存
+        bookDao.updateStock(isbn);
+        // 得到价格
+        Double price = bookDao.getPrice(isbn);
+        // 减余额
+        bookDao.updateBalance(username, price);
+    }
 }
